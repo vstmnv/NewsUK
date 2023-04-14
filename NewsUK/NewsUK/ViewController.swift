04ff02017457967c0d3f702e09860df3f38c761f
Vesela Stamenova <vstamenova@appolica.com>
2023-04-14 13:32:04 +0300

    Add: Created the project

---
+++ NewsUK/NewsUK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsUK
+//
+//  Created by Vesela Stamenova on 14.04.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
